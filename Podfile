a898cc3070b9b1c1a14b416e9764e6b9da350695
Mike Lewis <mlewis.mail@gmail.com>
2015-02-17 20:24:23 -0800

    skeleton

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '8.0'
+
+pod 'AFNetworking', '~> 2.5.0'
+pod 'BDBOAuth1Manager', '~> 1.5.0'
+
---
+++ twitter/Bridging-Header.h
@@ -0,0 +1,10 @@
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "AFNetworking.h"
+#import "UIImageView+AFNetworking.h"
+
+#import "NSDictionary+BDBOAuth1Manager.h"
+#import "BDBOAuth1RequestOperationManager.h"
+
+#endif
